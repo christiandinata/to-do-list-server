a271129a67c69281150ee7a603826bb2e29f5379
Christian Dinata <christiandinata@MacBook-Air.local>
2022-05-14 21:52:25 +0700

    fixed port

--- server.js
+++ server.js
@@ -30,10 +30,7 @@ app.enable("trust proxy");
 
 router(app);
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
